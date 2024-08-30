e79dda3d53736559996e19f3fd52513d84630f18
Abdo Nasser <abdonasser865@gmail.com>
2024-08-30 19:30:06 +0300

    add some useful scripts

---
+++ scripts/memory.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ 
+# get memory info
+MEMUSED=$(free --mebi | grep Mem | awk '{printf "%.2fG / %0.2fG", $3/1024, $2/1024}')
+
+# do the genmon
+echo "<txt>$MEMUSED</txt><txtclick>xfce4-taskmanager</txtclick>"
+echo "<tool> 
+free --mebi | grep Mem | awk '{printf "Memory: %d M of %d M used", $3, $2}'
+</tool>"
+ 
+exit 0
