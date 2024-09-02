31f8e4c41cd7cf9ec9d2926103b7a44f79858591
Abdo Nasser <abdonasser865@gmail.com>
2024-09-02 22:37:46 +0300

    many changes

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -2,11 +2,10 @@
  
 # get memory info
 MEMUSED=$(free --mebi | grep Mem | awk '{printf "%.2fG / %0.2fG", $3/1024, $2/1024}')
+MEMUSED2=$(free --mebi | grep Mem | awk '{printf "Memory: %d M of %d M used", $3, $2}')
 
 # do the genmon
 echo "<txt>$MEMUSED</txt><txtclick>gnome-system-monitor</txtclick>"
-echo "<tool> 
-free --mebi | grep Mem | awk '{printf "Memory: %d M of %d M used", $3, $2}'
-</tool>"
+echo "<tool>$MEMUSED2</tool>"
  
 exit 0
