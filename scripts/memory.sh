4f43e9acfff81fb1ecf87ee0f7b8df8f12535f80
Abdo Nasser <abdonasser865@gmail.com>
2024-10-08 20:48:58 +0300

    some changes

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -5,7 +5,7 @@ MEMUSED=$(free --mebi | grep Mem | awk '{printf "%.2fG / %0.2fG", $3/1024, $2/10
 MEMUSED2=$(free --mebi | grep Mem | awk '{printf "Memory: %d M of %d M used", $3, $2}')
 
 # do the genmon
-echo "<txt>$MEMUSED</txt><txtclick>gnome-system-monitor</txtclick>"
-echo "<tool>$MEMUSED2</tool>"
- 
-exit 0
+# echo "<txt>$MEMUSED</txt><txtclick>resources</txtclick>"
+# echo "<tool>$MEMUSED2</tool>"
+echo "<txt>Hello World!</txt>"
+
